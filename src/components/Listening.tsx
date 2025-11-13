e0cd5e3cf462e220783e9be3d5c26445e46dd162
jasmatp <parmarjasmat@gmail.com>
2025-11-13 09:57:51 +0530

    update title listening

--- src/components/Listening.tsx
+++ src/components/Listening.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { lestningLession } from "../mochData/lesteningData";
 import { Link } from "react-router-dom";
+import { Badge } from "react-bootstrap";
 
 const Listening = () => {
   return (
     <div className="container">
       {lestningLession.map((lesson, idx) => (
         <div key={idx} style={{ marginBottom: "1rem" }}>
-          <h2>{lesson.title || `Lesson ${idx + 1}`}</h2>
+          <h2 className="mt-2">
+            <Badge bg="secondary">English Listening Lessons</Badge>
+          </h2>
+          <p>Explore English listening lessons and improve your skills.</p>
           <Link
             to={`/listening/${idx}`}
             style={{ textDecoration: "underline", color: "blue" }}
